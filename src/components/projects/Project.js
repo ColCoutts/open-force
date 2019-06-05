8d9bd2cebbbc57a3ab236aa360b7c6c48f8ed483
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-05 11:46:17 -0700

    built a Project gonna test it

---
+++ src/components/projects/Project.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//bring in Link to get to Project Detail page (Link to={project.id})
+
+function Project({ project }) {
+  return (
+    <section>
+      <h1>{project.title}</h1>
+      <a href={project.url}>{project.url}</a>
+      <p>{project.summary}</p>
+    </section>
+  );
+}
+
+Project.propTypes = {
+  project: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired
+  })
+};
