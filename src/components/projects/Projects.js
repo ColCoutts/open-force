60733184a753d37c264f248e838272979a0b09a2
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-05 13:07:59 -0700

    projects component created

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Project from './Project';
+
+function Projects({ projects }) {
+  const projectList = projects.map(project => (
+    <li key={project.id}>
+      <Project project={project} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {projectList}
+    </ul>
+  );
+}
+
+Projects.propTypes = {
+  projects: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired
+  })).isRequired
+};
