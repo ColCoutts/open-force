bcdf726ae0ffc15cdca24a09b8c47967a30bd6e6
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-05 17:42:26 -0700

    more footer styling

--- src/components/footer/styled-footer/StyledFooter.js
+++ src/components/footer/styled-footer/StyledFooter.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer `
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 5rem;
   background-color: black;
-  
+
+  p {
+    color: white;
+  }
 `;
 
 export { StyledFooter };
