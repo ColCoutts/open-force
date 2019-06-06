d2bc43fd61a9bfe4fd3d453f867b7e2c83a04f7e
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-06 16:19:25 -0700

    styling sign up page

--- src/components/forms/SignUpForm.js
+++ src/components/forms/SignUpForm.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { postUser } from '../../actions/signUpActions';
 import { connect } from 'react-redux';
 import { getUserLoading, getUserError } from '../../selectors/userSelectors';
-
+import { StyledSignUpSection } from './StyledForms/StyledSignUp';
 
 export class SignUpForm extends PureComponent {
   static propTypes = {
@@ -41,14 +41,14 @@ export class SignUpForm extends PureComponent {
     const { username, password, email } = this.state;
 
     return (
-      <section>
+      <StyledSignUpSection>
         <form onSubmit={this.handleSubmit}>
           <input placeholder="username" name="username" type="text" value={username} onChange={this.handleChange}></input>
           <input placeholder="email" name="email" type="text" value={email} onChange={this.handleChange}></input>
           <input placeholder="password" name="password" type="text" value={password} onChange={this.handleChange}></input>
           <button>JOIN</button>
         </form>
-      </section>
+      </StyledSignUpSection>
     );
   }
 }
--- src/components/forms/StyledForms/StyledSignUp.js
+++ src/components/forms/StyledForms/StyledSignUp.js
@@ -1,1 +1,24 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const StyledSignUpSection = styled.section `
+  display: flex;
+  border: 5px solid yellow;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    input {
+      display: block;
+    }
+
+    button {
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
+
+export { StyledSignUpSection };
