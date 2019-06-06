eb96214f38158b454017740c1784aeea91ebace4
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-06 13:22:48 -0700

    signup actions created

--- src/actions/signUpActions.js
+++ src/actions/signUpActions.js
@@ -4,9 +4,9 @@ export const POST_USER = 'POST_USER';
 export const POST_USER_LOADING = 'POST_USER_LOADING';
 export const POST_USER_ERROR = 'POST_USER_ERROR';
 
-export const postUser = () => ({
+export const postUser = json => ({
   type: POST_USER,
   pendingType: POST_USER_LOADING,
   rejectedType: POST_USER_ERROR,
-  payload: createUser()
+  payload: createUser(json)
 });
