94f25a84affe2f5ce6d349c232f39071fbcf2437
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-06 13:18:33 -0700

    more actions

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,10 +1,8 @@
-//import api fetch once have a service/WAIT FOR ENDPOINT BEFORE CREATING THIS TEST
 import { getProjects } from '../services/backendFetch';
 
 export const FETCH_PROJECTS = 'FETCH_PROJECTS';
 export const FETCH_PROJECTS_LOADING = 'FETCH_PROJECTS_LOADING';
 export const FETCH_PROJECTS_ERROR = 'FETCH_PROJECTS_LOADING';
-
 
 export const fetchProjects = () => ({
   type: FETCH_PROJECTS,
---
+++ src/actions/signUpActions.js
@@ -0,0 +1,12 @@
+import { createUser } from '../services/backendFetch';
+
+export const POST_USER = 'POST_USER';
+export const POST_USER_LOADING = 'POST_USER_LOADING';
+export const POST_USER_ERROR = 'POST_USER_ERROR';
+
+export const postUser = () => ({
+  type: POST_USER,
+  pendingType: POST_USER_LOADING,
+  rejectedType: POST_USER_ERROR,
+  payload: createUser()
+});
